8cbcb039518a20f87c8223ac617d2892c96b93fd
Francisco Otero Medina <paquitotero@hotmail.com>
2018-09-23 21:33:56 -0500

    slider with img's

--- js/events.js
+++ js/events.js
@@ -11,53 +11,8 @@ function domSlider(idSlider) {
 
     let intervalId = setInterval(() => {
         nextNode.click();
-    }, 1000);
+    }, 3000);
 }
 
 domSlider('top-slider');
 domSlider('mid-slider');
-
-function getCharacters() {
-    let req = new XMLHttpRequest();
-    req.open('POST', 'https://rickandmortyapi.com/api/character/');
-    req.onreadystatechange = function () {
-        if(req.readyState == 4) {
-            console.log(req.responseText);
-        }
-    }
-    req.send();
-}
-
-const myPromise = new Promise((resolve, reject) => {
-    getCharacters();
-    let images = JSON.parse('[{"name": "Toxic Rick","url": "https://rickandmortyapi.com/api/character/avatar/361.jpeg"},{"name": "Morty Smith", "url": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"}]');
-
-    if (images.length < 1) {
-        reject('there are no images');
-    }
-    let bottomSliderNode = document.getElementById('bottom-slider');
-
-    images.forEach((img, index) => {
-        let liNode = document.createElement('li');
-        liNode.classList.add('slide');
-        if (index === 0) {
-            liNode.classList.add('visible');
-        }
-        let imgNode = document.createElement('img');
-        imgNode.setAttribute('src', img.url);
-        imgNode.setAttribute('alt', img.name);
-        liNode.appendChild(imgNode);
-        bottomSliderNode.appendChild(liNode);
-    });
-
-    resolve('Finished to build the slider images')
-});
-
-myPromise.then((msg) => {
-    console.log(msg);
-    domSlider('bottom-slider');
-});
-
-myPromise.catch(() => {
-    console.log('NO HE TERMINADO');
-});
